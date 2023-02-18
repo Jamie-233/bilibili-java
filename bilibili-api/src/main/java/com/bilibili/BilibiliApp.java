b0b0cbd2b80b1ebd3325b8d0164c57239d3ca9ea
Jenkin <zhaojiancun@yunquna.com>
2023-02-18 15:31:50 +0900

    chore: lint code

--- bilibili-api/src/main/java/com/bilibili/BilibiliApp.java
+++ bilibili-api/src/main/java/com/bilibili/BilibiliApp.java
@@ -2,11 +2,10 @@ package com.bilibili;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BilibiliApp {
     public static void main(String[] args) {
-        ApplicationContext app = SpringApplication.run(BilibiliApp.class, args);
+        SpringApplication.run(BilibiliApp.class, args);
     }
 }
